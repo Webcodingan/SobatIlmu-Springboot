999129f86a0085a2c1e1549fd27c0979805f57d9
liudaniel2201@gmail.com <liudaniel2201@gmail.com>
2025-01-31 19:58:46 +0700

    first

---
+++ src/main/java/com/sobatIlmu/model/ChatRequest.java
@@ -0,0 +1,21 @@
+package com.sobatIlmu.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRequest {
+
+    private String model;
+    private List<Message> messages;
+    private int n;
+    private double temperature;
+}
+
